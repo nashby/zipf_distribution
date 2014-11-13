160e5a7e693b049dc32cd98d41f39feb560fbc8c
Vasiliy Ermolovich <younash@gmail.com>
2014-11-13 15:51:38 +0300

    Require stop words and use plotter.

--- lib/zipf_distribution.rb
+++ lib/zipf_distribution.rb
@@ -1,8 +1,9 @@
-#require_relative 'plotter'
+require_relative 'stop_words'
+require_relative 'plotter'
 
 module Zipf
   class Distribution
-    #include Zipf::Plotter
+    include Zipf::Plotter
 
     def initialize(path_to_file)
       @path_to_file = path_to_file
